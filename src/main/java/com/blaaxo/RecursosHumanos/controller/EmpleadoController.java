66f08404fec59ba1ca09eb1539f9d36b37fd08a2
Blaaaxo <fernandezb@tecnocomfenalco.edu.co>
2024-06-13 20:21:50 -0500

    delete empleado funcional

--- src/main/java/com/blaaxo/RecursosHumanos/controller/EmpleadoController.java
+++ src/main/java/com/blaaxo/RecursosHumanos/controller/EmpleadoController.java
@@ -7,10 +7,14 @@ import com.blaaxo.RecursosHumanos.service.IEmpleadoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("rh-api")
@@ -51,5 +55,26 @@ public class EmpleadoController {
     public ResponseEntity<Empleado> update(@PathVariable Integer id, @RequestBody EmpleadoDTO empleadoDTO) {
         Empleado empleado = empleadoService.updateEmpleado(id, empleadoDTO);
         return ResponseEntity.ok(empleado);
+    }
+
+    @DeleteMapping("/empleados/delete/{id}")
+    public ResponseEntity<Map<String, Boolean>> delete(@PathVariable Integer id) {
+
+        //verificamos que el empleado existe
+        Empleado empleado = empleadoService.getEmpleadoById(id);
+
+        //si no existe mandamos una exepcion
+        if (empleado == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "El empleado con el id: " + id + " no existe"
+            );
+        }
+
+        empleadoService.deleteEmpleado(empleado);
+
+        // mapeamos la respuesta del Json {"eliminado": "true"}
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 }
